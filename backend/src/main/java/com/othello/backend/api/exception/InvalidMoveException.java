6146a90fb2af8ad0e908c31acea8ab9579bba885
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-03-10 18:31:02 -0400

    fix: Preserve error messages in Railway deployment by adding HTTP status codes

--- backend/src/main/java/com/othello/backend/api/exception/InvalidMoveException.java
+++ backend/src/main/java/com/othello/backend/api/exception/InvalidMoveException.java
@@ -1,5 +1,9 @@
 package com.othello.backend.api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidMoveException extends RuntimeException {
     public InvalidMoveException() {
         super("Invalid move");
